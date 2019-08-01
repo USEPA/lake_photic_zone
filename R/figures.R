69ec1884afbc803bcf4ff5fdb32d932ae9b57978
Betty Kreakie <b.kreakie@gmail.com>
2019-08-01 14:43:01 -0400

    Added rough draft of figures to the manuscript

--- R/figures.R
+++ R/figures.R
@@ -3,10 +3,35 @@
 #' 
 source(here::here("R/functions.R"))
 
-
+###Variable selection figure 
 varsel_fig<-varsel_plot(var_sel_tmean)
 
-ggsave(here::here("figures/valselfig.jpg"), varsel_fig, width = 7.5, height = 5.625, 
+ggsave(here::here("figures/varSelFig.jpg"), varsel_fig, width = 7.5, height = 5.625, 
        units = "in", dpi = 600)
+
+
+###Variable importance figure
+
+jpeg("figures/varImpPlot.jpg")
+varImpPlot(RFAll,type=1,main="")
+dev.off()
+
+
+
+###Partial Dependence Plots 
+jpeg("figures/partPlot.jpg")
+par(mfrow=c(4,2))
+
+partialPlot(RFAll, nla.all,Date,main="")
+partialPlot(RFAll, nla.all,Average.Temperature,main="")
+partialPlot(RFAll, nla.all,Longitude,main="")
+partialPlot(RFAll, nla.all,Thirty.Day.Average.Temperature,main="")
+partialPlot(RFAll, nla.all,Elevation,main="")
+partialPlot(RFAll, nla.all,Latitude,main="")
+partialPlot(RFAll, nla.all,Surface.Area,main="")
+partialPlot(RFAll, nla.all,Shoreline.Length,main="")
+dev.off()
+
+
 
 
---
+++ R/varImportance.R
@@ -0,0 +1,20 @@
+#' Variable Importance Summary
+#' 
+#' @param rf_imp a Random Forest Importance Object
+#' @param label A label for the model
+#' @export
+varImportance<-function(rf_imp,label=NULL){
+  
+  if(!is.null(label)){
+    xdf<-data.frame(model_id=rep(label,nrow(rf_imp)),
+                    variable_names=row.names(rf_imp),
+                    mean_decrease_acc=rf_imp[,1],
+                    mean_decrease_gini=rf_imp[,2])
+  } else {
+    xdf<-data.frame(model_id=rep(NA,nrow(rf_imp)),
+                    variable_names=row.names(rf_imp),
+                    mean_decrease_acc=rf_imp[,1],
+                    mean_decrease_gini=rf_imp[,2])
+  }   
+  return(xdf)
+}
